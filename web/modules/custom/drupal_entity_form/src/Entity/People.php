b90a8fa1ab73672f5760c69c9d690665811ee7ce
arielbarrios <arielbarrios94@gmail.com>
2024-10-05 19:23:37 -0300

    modificacion de campos people

--- web/modules/custom/drupal_entity_form/src/Entity/People.php
+++ web/modules/custom/drupal_entity_form/src/Entity/People.php
@@ -10,6 +10,7 @@ use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Entity\EntityTypeInterface;
 use Drupal\Core\Field\BaseFieldDefinition;
 use Drupal\drupal_entity_form\PeopleInterface;
+use Drupal\Tests\Core\Plugin\ConfigurablePlugin;
 use Drupal\user\EntityOwnerTrait;
 
 /**
@@ -98,24 +99,32 @@ final class People extends ContentEntityBase implements PeopleInterface
       ])
       ->setDisplayConfigurable('view', true);
 
-    $fields['documents'] = BaseFieldDefinition::create('file')
-      ->setLabel(t('Attached Documents'))
-      ->setSettings([
-        'uri_scheme' => 'public',
-        'file_directory' => '',
-        'file_extensions' => 'png jpg jpeg pdf',
-      ])
-      ->setRevisionable(true)
-      ->setDisplayOptions('view', array(
-        'label' => 'above',
-        'weight' => 0,
-      ))
-      ->setDisplayOptions('form', array(
-        'weight' => 0,
-      ))
-      ->setDisplayConfigurable('form', true)
-      ->setDisplayConfigurable('view', true);
 
+
+    // Image (New Field)
+    $fields['image'] = BaseFieldDefinition::create('image')
+      ->setLabel(t('Profile Image'))
+      ->setDescription(t('The profile image of the user.'))
+      ->setSettings([
+        'file_extensions' => 'png jpg jpeg', // Define los tipos de imágenes permitidos
+        'alt_field' => TRUE, // Habilita campo para el texto alternativo
+        'alt_field_required' => TRUE,
+      ])
+      ->setDisplayOptions('form', [
+        'type' => 'image_image',
+        'weight' => 0,
+      ])
+      ->setDisplayOptions('view', [
+        'type' => 'image',
+        'weight' => 0,
+        'settings' => [
+          'image_style' => 'thumbnail', // Ajusta el estilo de imagen, si está configurado
+          'image_link' => 'content',
+        ],
+      ])
+      ->setRequired(FALSE);
+
+    // Email
     $fields['email'] = BaseFieldDefinition::create('email')
       ->setLabel(t('Email'))
       ->setDescription(t('The email address of the user.'))
@@ -136,6 +145,105 @@ final class People extends ContentEntityBase implements PeopleInterface
       ])
       ->setDisplayConfigurable('view', true)
       ->setDisplayConfigurable('form', true);
+
+
+    // Date of Birth
+    $fields['date_of_birth'] = BaseFieldDefinition::create('datetime')
+      ->setLabel(t('Date of Birth'))
+      ->setDescription(t('The date of birth of the user.'))
+      ->setSettings([
+        'datetime_type' => 'date',
+      ])
+      ->setRequired(TRUE)
+      ->setDisplayConfigurable('view', true)
+      ->setDisplayConfigurable('form', true);
+
+    // Gender
+    $fields['gender'] = BaseFieldDefinition::create('list_string')
+      ->setLabel(t('Gender'))
+      ->setDescription(t('The gender of the user.'))
+      ->setSettings([
+        'allowed_values' => [
+          'male' => 'Male',
+          'female' => 'Female',
+          'other' => 'Other',
+        ],
+      ])
+      ->setRequired(TRUE)
+      ->setDisplayConfigurable('view', true)
+      ->setDisplayConfigurable('form', true);
+
+    // Address
+    $fields['address'] = BaseFieldDefinition::create('string_long')
+      ->setLabel(t('Address'))
+      ->setDescription(t('The address of the user.'))
+      ->setSettings([
+        'max_length' => 255,
+        'text_processing' => 0,
+      ])
+      ->setDisplayConfigurable('view', true)
+      ->setDisplayConfigurable('form', true);
+
+
+    // Phone Number
+    $fields['phone_number'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('Phone Number'))
+      ->setDescription(t('The phone number of the user.'))
+      ->setRequired(TRUE)
+      ->setDisplayConfigurable('view', true)
+      ->setDisplayConfigurable('form', true);
+
+    // Country (Entity Reference)
+    $fields['country_of_residence'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('Country of Residence'))
+      ->setDescription(t('The country of residence of the user.'))
+      ->setSetting('target_type', 'taxonomy_term')
+      ->setSetting('handler', 'default')
+      ->setSetting('handler_settings', [
+        'target_bundles' => ['countries'], // Aquí asegúrate de que 'countries' es el vocabulario que creaste
+      ])
+      ->setRequired(TRUE)
+      ->setDisplayOptions('form', [
+        'type' => 'options_select',
+        'weight' => 0,
+      ])
+      ->setDisplayOptions('view', [
+        'label' => 'hidden',
+        'type' => 'taxonomy_term_reference_label',
+        'weight' => 0,
+      ])
+      ->setDisplayConfigurable('view', true)
+      ->setDisplayConfigurable('form', true);
+
+    // Terms and Conditions
+    $fields['terms_and_conditions'] = BaseFieldDefinition::create('boolean')
+      ->setLabel(t('Terms and Conditions'))
+      ->setDescription(t('Indicates whether the user has accepted the terms and conditions.'))
+      ->setDefaultValue(FALSE)
+      ->setRequired(TRUE)
+      ->setDisplayConfigurable('view', true)
+      ->setDisplayConfigurable('form', true);
+
+
+    $fields['documents'] = BaseFieldDefinition::create('file')
+      ->setLabel(t('Attached Documents'))
+      ->setSettings([
+        'uri_scheme' => 'public',
+        'file_directory' => '',
+        'file_extensions' => 'png jpg jpeg pdf',
+      ])
+      ->setRevisionable(true)
+      ->setDisplayOptions('view', array(
+        'label' => 'above',
+        'weight' => 0,
+      ))
+      ->setDisplayOptions('form', array(
+        'weight' => 0,
+      ))
+      ->setDisplayConfigurable('form', true)
+      ->setDisplayConfigurable('view', true);
+
+
 
 
     $fields['acceptance_of_terms_and_conditions'] = BaseFieldDefinition::create('boolean')
